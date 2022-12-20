e58554d974e5013812439f8e359e8cd18eccff45
Dhananjayan <dhananjayan.online@outlook.com>
2022-12-20 15:32:31 +0530

    base page added and index page linked to base page

--- web/urls.py
+++ web/urls.py
@@ -3,5 +3,5 @@ from .views import SignInView,SignUpView,IndexView
 urlpatterns = [
     path("",SignInView.as_view(),name="signin"),
     path("register",SignUpView.as_view(),name="signup"),
-    path("index",IndexView.as_view(),name="index")
+    path("index",IndexView.as_view(),name="index"),
 ]
